094a3ca462b810a34a46187dbd0a1bcb9bbc9a36
Albert <albert.nasyrov2016@gmail.com>
2019-11-10 21:20:38 +0300

    Проблема ромбовидного наследования

---
+++ Interfaces/src/Pechka.java
@@ -0,0 +1,10 @@
+public class Pechka implements Krovat, Duhovka{
+    @Override
+    public void youMaySleep(){
+        System.out.println("Hr-hr-hr");
+    };
+    @Override
+    public void canCookMeal(){
+        System.out.println("Я отравился");
+    };
+}
