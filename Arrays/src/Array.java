20e80e2c8f9e7a38946de96594111b54524b4f43
Albert <albert.nasyrov2016@gmail.com>
2019-11-13 22:08:03 +0400

    Исправил Bubblesort, добавил insertionSort

--- Arrays/src/Array.java
+++ Arrays/src/Array.java
@@ -10,12 +10,14 @@ public class Array {
         doublearray = new double[80];
         int[] intarray1 = intArrayCreator(intarray);
         int[] intarray2 = bubbleSort(intarray1);
-
+        long[] longarray1 = longArrayCreator(longarray);
+        long[] longarray2 = insertionSort(longarray1);
         for (int i = 0; i < intarray2.length; i ++){
             System.out.print(intarray2[i] + " ");
         }
-        for (int i = 0; i < intarray2.length; i ++){
-            System.out.print(intarray2[i] + " ");
+        System.out.println();
+        for (int i = 0; i < longarray2.length; i ++){
+            System.out.print(longarray2[i] + " ");
         }
     }
     public static int[] bubbleSort(int[] intarray1){
@@ -23,8 +25,8 @@ public class Array {
         int j;
         while (!sorted){
             sorted = true;
-            for (int i = intarray1.length - 1; i < 0; i ++){
-                if (intarray1[i] > intarray1[i + 1]){
+            for (int i = intarray1.length - 1; i > 0; i --){
+                if (intarray1[i] < intarray1[i - 1]){
                     j = intarray1[i];
                     intarray1[i] = intarray1[i - 1];
                     intarray1[i - 1] = j;
@@ -33,6 +35,18 @@ public class Array {
             }
         }
         return intarray1;
+    }
+    public static long[] insertionSort(long[] longarray) {
+        for (int i = 1; i < longarray.length; i++) {
+            long current = longarray[i];
+            int j = i - 1;
+            while(j >= 0 && current < longarray[j]) {
+                longarray[j+1] = longarray[j];
+                j --;
+            }
+            longarray[j+1] = current;
+        }
+        return longarray;
     }
     public static int[] intArrayCreator(int intarray[]) {
         for (int q = 0; q < intarray.length; q++){
